740548c4d866959f9d2f7db3d05ce6f80a69988e
rmakarenko <roman.makarenko.spb@gmail.com>
2022-04-12 17:40:05 +0300

    Update linked_list_tests.py

--- linked_list_tests.py
+++ linked_list_tests.py
@@ -1,8 +1,8 @@
 import unittest
 
-from linked2 import Node
+from linked_from_git import Node
 
-from linked2 import LinkedList
+from linked_from_git import LinkedList
 
 class LinkedListTests(unittest.TestCase):
 
@@ -19,6 +19,46 @@ class LinkedListTests(unittest.TestCase):
         self.s_list.add_in_tail(Node(3))
         self.s_list.add_in_tail(Node(3))
         self.s_list.add_in_tail(Node(4))
+
+        self.s_list3 = LinkedList()
+        self.s_list3.add_in_tail(Node(0))
+        self.s_list3.add_in_tail(Node(0))
+        self.s_list3.add_in_tail(Node(1))
+        self.s_list3.add_in_tail(Node(1))
+        self.s_list3.add_in_tail(Node(1))
+        self.s_list3.add_in_tail(Node(2))
+        self.s_list3.add_in_tail(Node(2))
+
+    def test_delete_00111222_check(self):
+
+        self.s3_list = LinkedList()
+        self.s3_list.add_in_tail(Node(0))
+        self.s3_list.add_in_tail(Node(0))
+        self.s3_list.add_in_tail(Node(1))
+        self.s3_list.add_in_tail(Node(1))
+        self.s3_list.add_in_tail(Node(1))
+        self.s3_list.add_in_tail(Node(2))
+        self.s3_list.add_in_tail(Node(2))
+
+        self.s3_list.delete(1, True)  # останется 0 0 2 2
+        self.s3_list.delete(2, True)  # останется 0 0
+
+        self.assertEqual(self.s3_list.head.next, self.s3_list.tail, "Next элемента head не указывает на второй элемент")
+        self.assertEqual(self.s3_list.tail.next, None, "Next элемента tail не указывает на None")
+
+        self.assertEqual(self.s3_list.head.value, 0, "Value элемента head не равно 0")
+        self.assertEqual(self.s3_list.tail.value, 0, "Value элемента tail не равно 0")
+
+        self.s3_list.delete(0, False)  # будет удален первый ноль, останется один элемент ноль
+        self.assertEqual(self.s3_list.head.value, 0, "Value элемента head не равно 0")
+        self.assertEqual(self.s3_list.tail.value, 0, "Value элемента tail не равно 0")
+        self.assertEqual(self.s3_list.head.next, None, "Next элемента head не указывает на None")
+        self.assertEqual(self.s3_list.tail.next, None, "Next элемента tail не указывает на None")
+        self.s3_list.delete(0, False)  # будет удален первый ноль, не останется ничего
+        self.assertEqual(self.s3_list.head, None, "head не none")
+        self.assertEqual(self.s3_list.tail, None, "tail не none")
+        # self.assertEqual(self.s3_list.head.next, None, "Next элемента head не указывает на None")
+        # self.assertEqual(self.s3_list.tail.next, None, "Next элемента tail не указывает на None")
 
     def test_find_all(self):
 
@@ -37,6 +77,8 @@ class LinkedListTests(unittest.TestCase):
         self.assertEqual(self.s2_list.len(), 1, "Значение длины после удаления некорректно")
         self.assertEqual(self.s2_list.head.next, None, "Next элемента head не указывает на None")
         self.assertEqual(self.s2_list.tail.next, None, "Next элемента tail не указывает на None")
+        self.assertEqual(self.s2_list.head.value, 2, "Значение элемента head некорректно")
+        self.assertEqual(self.s2_list.tail.value, 2, "Значение элемента tail некорректно")
 
     def test_delete(self):
 
@@ -48,8 +90,10 @@ class LinkedListTests(unittest.TestCase):
         self.assertEqual(self.s_list.head.value, 3, "Значение элемента head некорректно")
         self.assertEqual(self.s_list.tail.value, 3, "Значение элемента tail некорректно")
         self.s_list.delete(3, False)
-        self.assertEqual(self.s_list.head, None, "Значение элемента head некорректно")
-        self.assertEqual(self.s_list.tail, None, "Значение элемента tail некорректно")
+        self.assertEqual(self.s_list.head.value, 3, "Значение элемента head некорректно")
+        self.assertEqual(self.s_list.tail.value, 3, "Значение элемента tail некорректно")
+        self.assertEqual(self.s_list.head.next, None, "Значение указателя хвоста некорректно")
+
 
     def test_delete_tail_false(self):
 
