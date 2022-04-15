f64b53bb4ec65e9c9ac428c8b8c06ea3e7528d03
rmakarenko <roman.makarenko.spb@gmail.com>
2022-04-15 16:54:23 +0300

    Update LinkedList1.py

--- LinkedList1.py
+++ LinkedList1.py
@@ -91,43 +91,18 @@ class LinkedList:
 
     def insert(self, afterNode, newNode):
 
+        target_node = None
         if self.len() == 0:
-            
-            node_for_insert = Node(newNode)
-            node_for_insert.next = None
-            self.head = node_for_insert
-            self.tail = node_for_insert
-            
-            return 
-
-
-        if afterNode is None and self.len() == 0:
-
-            node_for_insert = Node(newNode)
-            node_for_insert.next = None
-            self.head = node_for_insert
-            self.tail = node_for_insert
-
-        elif afterNode is None and self.len() != 0:
-
-            node_ex_head = self.head
             self.head = Node(newNode)
-            newNode.next = node_ex_head
-
+            self.tail = self.head
         else:
-            node_for_insert = Node(newNode)  # создать новый узел
-            node = self.head  # найти узел афтернод и сохранить в буфер его следующий элемент, присвоить ему следующий элемент = новый узел
-
-            while node is not None:
-                if node.value == afterNode:
-                    buffered_item = node.next
-                    node.next = node_for_insert
-                    node_for_insert.next = buffered_item
-                    break
-                else:
-                    node = node.next
-
-            if self.tail.value == afterNode:
-                self.tail = node_for_insert 
-    
+            target_node = self.find(afterNode)
+        if target_node is None:
+            return
+        target_next = target_node.next
+        target_node.next = Node(newNode)
+        target_node.next.next = target_next
+        if target_next is None:
+            self.tail = target_node.next    
+   
 
